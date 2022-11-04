916a1f52eca08be8df676bd3ca47ce59317fdf23
won-jun-choi <choi152552@gmail.com>
2022-11-04 15:53:59 -0400

    update RunAll.R

--- assignments/assignment3/code/RunAll.R
+++ assignments/assignment3/code/RunAll.R
@@ -5,8 +5,10 @@ source('data.R')
 
 # tables
 source('tab_descstat.R') # Q1
+source('tab_table3.R')  # Q6
 
 # figures
 source('fig_premium_enrolshare.R') # Q2
 source('fig_bin10_bin30.R') # Q3
 source('fig_binopt.R')  # Q4
+source('fig_rddensity.R')  # Q5
